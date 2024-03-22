35350c98dc3735d4eacf87be9bdb56cbc858119c
Amiidus <161928031+FromZeroToHeroAmiidus@users.noreply.github.com>
2024-03-22 09:10:42 +0100

    added express support and readme.txt

--- Node.js Express MongoDB/index.js
+++ Node.js Express MongoDB/index.js
@@ -1,23 +1,45 @@
 const http = require("http");
 
 const fs = require("fs");
-const homePage = fs.readFileSync("index.html");
-const aboutPage = fs.readFileSync("about.html");
-const contactPage = fs.readFileSync("contact.html");
-const notFoundPage = fs.readFileSync("notfound.html");
 
-const server = http.createServer((req,res) =>{
-    console.log(req.url);
-    if (req.url === "/about")
-        res.end(aboutPage)
-    else if (req.url === "/contact")
-        res.end(contactPage)
-    else if (req.url === "/")
-        res.end(homePage)
-    else {
-        res.writeHead(404);
-        res.end(notFoundPage);
-    }
+const express = require("express");
+
+// za express
+const path = require("path");
+
+// const homePage = fs.readFileSync("index.html");
+// const aboutPage = fs.readFileSync("about.html");
+// const contactPage = fs.readFileSync("contact.html");
+// const notFoundPage = fs.readFileSync("notfound.html");
+
+// const server = http.createServer((req,res) =>{
+//     console.log(req.url);
+//     if (req.url === "/about")
+//         res.end(aboutPage)
+//     else if (req.url === "/contact")
+//         res.end(contactPage)
+//     else if (req.url === "/")
+//         res.end(homePage)
+//     else {
+//         res.writeHead(404);
+//         res.end(notFoundPage);
+//     }
+// })
+
+// server.listen(3000);
+
+// da probamo express ... 
+const app = express(); // calls express function to start new Express app
+app.listen(3000,()=> {
+    console.log("App listening on port 3000");
 })
 
-server.listen(3000);
+app.get('/about',(req,res)=>{
+    res.json({
+    name: 'Greg Lim'
+    })
+})
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"index.html"));
+})
---
+++ Node.js Express MongoDB/readme.txt
@@ -0,0 +1,12 @@
+Bismillah rahman i rahim
+
+Aktivnost : Prebrati knjigo. Jo probati razumeti in pretipkati dano vsebino.
+	    Vmes se spraševati zakaj? Kako? In si spisati morebitne zapiske.
+	    
+	    
+Avtor in knjiga : Greg Lim - Beginning Node.js, Express & MongoDB Development (&c)
+Leto izdaje     : 2019
+
+Tehnologije : Zapisane v naslovu knjige
+
+
