817456e64fab23473754f18019b977703f96608e
Amiidus <161928031+FromZeroToHeroAmiidus@users.noreply.github.com>
2024-03-26 15:35:24 +0100

    added react default files

---
+++ React/React Crash Course 2024/react-crash-2024/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const name = "John";
+
+const App = () => {
+  return (
+    <>
+      <div className='text-5xl'>App</div>
+      <p>Hello {name}</p>
+    </>
+  )
+}
+
+export default App
