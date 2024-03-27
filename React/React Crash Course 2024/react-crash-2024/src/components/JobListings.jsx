b28ddd7c4ba0d5c06cd25a806dd645bbe0a67600
Amiidus <161928031+FromZeroToHeroAmiidus@users.noreply.github.com>
2024-03-27 13:43:30 +0100

    added loading spinner and proxy config

--- React/React Crash Course 2024/react-crash-2024/src/components/JobListings.jsx
+++ React/React Crash Course 2024/react-crash-2024/src/components/JobListings.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 // hocem side-efekt
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 const JobListings = ({isHome = false}) => {
 
@@ -12,8 +13,10 @@ const JobListings = ({isHome = false}) => {
   useEffect(() => {
     // fetching tle
     const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : 
+      '/api/jobs'
       try {
-        const res = await fetch('http://localhost:8000/jobs');
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       }catch (error) {
@@ -31,18 +34,18 @@ const JobListings = ({isHome = false}) => {
       <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
         {isHome ? "Recent Jobs" : "Browse Jobs"}
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
         {
-          loading ? (<h2>Loading...</h2>) : // ce loada ...
-          <>
+          loading ? (<Spinner loading={loading}/>) : // ce loada ...
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {jobs.map((job) => ( // ce je zloadalo ...  
             <JobListing key={job.id} job={job} />
         ))}
-         </>
+         </ div>
         }
 
 
-      </div>
+
     </div>
   </section>
   )
