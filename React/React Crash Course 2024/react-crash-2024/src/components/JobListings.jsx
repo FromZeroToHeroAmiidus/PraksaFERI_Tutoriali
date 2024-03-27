37add9f94d521d12e6d36769a9943dc9be88dce6
Amiidus <161928031+FromZeroToHeroAmiidus@users.noreply.github.com>
2024-03-27 12:48:15 +0100

    added Json server support

--- React/React Crash Course 2024/react-crash-2024/src/components/JobListings.jsx
+++ React/React Crash Course 2024/react-crash-2024/src/components/JobListings.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import jobs from '../jobs.json'
+// hocem side-efekt
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
 
 const JobListings = ({isHome = false}) => {
 
-  const jobListings = isHome ? jobs.slice(0,3) : jobs; // ce hocem v tem primeru zgolj tri jobe
+  //const jobListings = isHome ? jobs.slice(0,3) : jobs; // ce hocem v tem primeru zgolj tri jobe
+  const [jobs, setJobs] = useState([]); // vzame ime state-a in funkcijo state-a
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // fetching tle
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      }catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchJobs();
+  },[]);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -13,9 +32,15 @@ const JobListings = ({isHome = false}) => {
         {isHome ? "Recent Jobs" : "Browse Jobs"}
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {jobListings.map((job) => (
+        {
+          loading ? (<h2>Loading...</h2>) : // ce loada ...
+          <>
+          {jobs.map((job) => ( // ce je zloadalo ...  
             <JobListing key={job.id} job={job} />
         ))}
+         </>
+        }
+
 
       </div>
     </div>
