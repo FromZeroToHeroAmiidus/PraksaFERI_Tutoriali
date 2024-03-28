f5fce9886f765deef6d55fb64e9e09786829272c
Amiidus <161928031+FromZeroToHeroAmiidus@users.noreply.github.com>
2024-03-28 14:51:04 +0100

    added single page

--- React/React Crash Course 2024/react-crash-2024/src/App.jsx
+++ React/React Crash Course 2024/react-crash-2024/src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage, { jobLoader } from './pages/JobPage';
 
 
 const router = createBrowserRouter(
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
     <Route index element={<HomePage />}/>
     <Route path="/jobs" element={<JobsPage />}/>
     <Route path="*" element={<NotFoundPage />}/>
+    <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}/>
   </Route>
   )
 );
@@ -24,3 +26,6 @@ const App = () => {
 };
 
 export default App
+
+// t1 : npm run dev
+// t2 : npm run server
