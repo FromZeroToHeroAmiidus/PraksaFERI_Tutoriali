1a2aa0fdca68b5b3534c3cd65b8adccc96927935
Amiidus <161928031+FromZeroToHeroAmiidus@users.noreply.github.com>
2024-03-28 16:15:46 +0100

    added option to delete selected job listing

--- React/React Crash Course 2024/react-crash-2024/src/App.jsx
+++ React/React Crash Course 2024/react-crash-2024/src/App.jsx
@@ -13,7 +13,7 @@ import AddJobPage from './pages/AddJobPage';
 
 
 const App = () => {
-
+// Add new job
   const addJob = async (newJob) => {
     const res = await fetch('/api/jobs',{
       method: 'POST',
@@ -25,13 +25,21 @@ const App = () => {
     return;
   }
 
+// Delete job
+const deleteJob = async (id) => {
+  const res = await fetch(`/api/jobs/${id}`,{
+    method: 'DELETE',
+  });
+  return;
+}
+
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout/>}>
     <Route index element={<HomePage />}/>
     <Route path="/jobs" element={<JobsPage />}/>
     <Route path="*" element={<NotFoundPage />}/>
-    <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader}/>
+    <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader={jobLoader}/>
     <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob}/>}/>
   </Route>
   )
--- React/React Crash Course 2024/react-crash-2024/src/pages/JobPage.jsx
+++ React/React Crash Course 2024/react-crash-2024/src/pages/JobPage.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 // import { useState, useEffect } from 'react'
-import { useParams, useLoaderData } from 'react-router-dom';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 
-const JobPage = () => {
+const JobPage = ({deleteJob}) => {
+  const navigate = useNavigate();
   const {id} = useParams();
   const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm("Are you sure you want to delete this listing?");
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    navigate('/jobs');
+  }
   // const [job, setJob] = useState(null);
   // const [loading, setLoading] = useState(true);
 
@@ -108,7 +118,7 @@ const JobPage = () => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link
               >
-              <button
+              <button onClick={() => onDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
