c17d23e382b3cf9251aef687dbcfd60b9fb4d77b
Amiidus <amiidus@pwnator.com>
2024-03-30 15:21:10 +0100

    added global pipes usage

--- nestjs-app/src/auth/auth.controler.ts
+++ nestjs-app/src/auth/auth.controler.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, ParseIntPipe, Post} from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
@@ -8,8 +9,8 @@ export class AuthController{
     //endpoint
     // POST /auth/signup
     @Post('signup')
-    signup() {
-        return this.authService.signup();
+    signup(@Body() dto: AuthDto) {
+      return this.authService.signup();
     }
 
     @Post('signin')
