6312d1c81c7ea773dc2b23945761f301042ad31d
Amiidus <amiidus@pwnator.com>
2024-03-30 13:03:53 +0100

    project is ready for coding

--- nestjs-app/src/auth/auth.service.ts
+++ nestjs-app/src/auth/auth.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
-@Injectable({})
+
+@Injectable()
 export class AuthService {
+
+
+    constructor(private prisma: PrismaService) {
+
+    }
     
     signup() {
         return { msg: "Hello" }
